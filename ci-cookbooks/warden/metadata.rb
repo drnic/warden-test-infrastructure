38b65cbe847b5b0590b826a7a0ae7218a2e18ee8
Dr Nic Williams <drnicwilliams@gmail.com>
2013-04-30 12:08:35 -0700

    metadata.rb file for the two chef cookbooks

---
+++ ci-cookbooks/warden/metadata.rb
@@ -0,0 +1,6 @@
+name "warden"
+version "1.0.0"
+description "Install warden"
+long_description "Install warden into /warden/warden"
+
+supports "ubuntu"
---
+++ ci-cookbooks/zip/metadata.rb
@@ -0,0 +1,4 @@
+name "zip"
+version "1.0.0"
+description "Install zip & unzip packages"
+long_description "Install zip & unzip packages"
